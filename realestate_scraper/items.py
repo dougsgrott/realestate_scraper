2b9daad83b068639f82c7d00eb2eb56b8b2fcbf7
Doug <doug.sgrott@gmail.com>
2020-11-21 23:51:01 -0300

    added ItemLoader

--- realestate_scraper/items.py
+++ realestate_scraper/items.py
@@ -4,6 +4,79 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import Compose, TakeFirst, Join
+import re
+
+def cleanText(text):
+    processed_text = []
+    for subtext in text:
+        subtext = subtext.replace('\n', '').replace('\t', '')
+        #subtext = re.split('-| - |,|, ', subtext)
+        subtext = subtext.lstrip()
+        subtext = subtext.rstrip()
+        
+        processed_text.append(subtext)
+
+    return processed_text
+
+def cleanAndBreakText(text):
+    for subtext in text:
+        subtext = subtext.replace('\n', '').replace('\t', '')
+        subtext = re.split('-| - |,|, ', subtext)
+        subtext = [string.lstrip() for string in subtext]
+        subtext = [string.rstrip() for string in subtext]
+    return text
+
+class ImoveisSCItem(scrapy.Item):
+    title = scrapy.Field(
+        input_processor=cleanText,
+        output_processor=TakeFirst()
+    )
+    code = scrapy.Field(
+        input_processor=cleanText,
+        output_processor=TakeFirst()
+    )
+
+    price = scrapy.Field(
+        input_processor=cleanText,
+        output_processor=TakeFirst()
+    )
+
+    caracteristicas_simples = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    #var1_name_list = scrapy.Field()
+    #var1_value_list = scrapy.Field()
+    
+    description_header = scrapy.Field(
+        input_processor=cleanText,
+        output_processor=TakeFirst()
+    )
+
+    description = scrapy.Field(
+        input_processor=Compose(cleanText, Join(separator='<br>')),
+        output_processor=TakeFirst()
+    )
+
+    #var2_subtitle_list = scrapy.Field()
+    #var2_value_list = scrapy.Field()
+    caracteristicas_detalhes = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    address = scrapy.Field(
+        input_processor=cleanText,
+        output_processor=TakeFirst()
+    )
+
+    advertiser = scrapy.Field(output_processor=TakeFirst())
+
+    url = scrapy.Field(output_processor=TakeFirst())
+    date_year = scrapy.Field(output_processor=TakeFirst())
+    date_month = scrapy.Field(output_processor=TakeFirst())
+    date_day = scrapy.Field(output_processor=TakeFirst())
+
+    
 
 
 class RealestateScraperItem(scrapy.Item):
--- realestate_scraper/spiders/imoveis_sc_spider.py
+++ realestate_scraper/spiders/imoveis_sc_spider.py
@@ -24,6 +24,11 @@ from scrapy.spiders import Spider
 from scrapy import Request
 from w3lib.html import remove_tags
 
+from datetime import datetime
+import sys
+
+sys.path.append("/home/user/PythonProj/Scraping/realestate_scraper/realestate_scraper")
+from items import ImoveisSCItem
 
 class ImoveisSCSpider(Spider):
     name = 'imoveis_sc'
@@ -43,43 +48,69 @@ class ImoveisSCSpider(Spider):
 
     # 2. SCRAPING LEVEL 2
     def populate_item(self, response):
-        #item_loader = ItemLoader(item=MySpiderItem(), response=response)
+        item_loader = ItemLoader(ImoveisSCItem(), response=response)        
         #item_loader.default_input_processor = MapCompose(remove_tags)
 
         # Header data
-        title = response.xpath('//*[@class="visualizar-title"]/text()').get()
-        code = response.xpath('//*[@class="visualizar-header-extra"]/strong/text()').get()
+        item_loader.add_xpath('title', '//*[@class="visualizar-title"]/text()') #title = response.xpath('//*[@class="visualizar-title"]/text()').getall()
+        item_loader.add_xpath('code', '//*[@class="visualizar-header-extra"]/strong/text()') #code = response.xpath('//*[@class="visualizar-header-extra"]/strong/text()').getall()
 
         # 'top' Section data
-        price = response.xpath('//*[@class="visualizar-preco  has-extra "]/strong/text()').get()
-
-        #iptu pt1 = response.xpath('//*[@class="visualizar-preco  has-extra "]/text()').getall()
-        #iptu pt2 = response.xpath('//*[@class="visualizar-preco  has-extra "]/span/text()').get()
-        #enredeco opt1 = response.xpath('//*[contains(@class, "icon-blue")]/../text()').getall()
-        #endereco opt2 = response.xpath('//*[@class="visualizar-info-location"]/text()').getall()
-
+        item_loader.add_xpath('price', '//*[contains(@class, "visualizar-preco")]/strong/text()') #price = response.xpath('//*[contains(@class, "visualizar-preco")]/strong/text()').getall()
         var1_name_list = []
         var1_value_list = []
+        caracteristicas_simples = {}
         for li in response.xpath('//ol[@class="visualizar-info-opcoes"]/li'):
-            var1_name_list.append(li.xpath('.//i').attrib['class'].split('-')[-1])
-            var1_value_list.append(li.xpath('./strong/text()').get())
+            key = li.xpath('.//i').attrib['class'].split('-')[-1]
+            value = li.xpath('./strong/text()').get()
+            caracteristicas_simples[key] = value
+            #var1_name_list.append(li.xpath('.//i').attrib['class'].split('-')[-1])
+            #var1_value_list.append(li.xpath('./strong/text()').get())
+        
+        item_loader.add_value('caracteristicas_simples', caracteristicas_simples)
+        #item_loader.add_value('var1_name_list', var1_name_list)
+        #item_loader.add_value('var1_value_list', var1_value_list)
+
 
         # 'descricao' Section data
-        description_header = response.xpath('//*[@class="visualizar-descricao"]/h2/text()').getall()
-        description = response.xpath('//*[@class="visualizar-descricao"]/p/text()').getall()
+        #description_header = response.xpath('//*[@class="visualizar-descricao"]/descendant::*/text()').getall()[0]
+        #description = response.xpath('//*[@class="visualizar-descricao"]/descendant::*/text()').getall()[1:]
+        item_loader.add_xpath('description', '//*[@class="visualizar-descricao"]/descendant::*/text()')
+        item_loader.add_xpath('description_header', '//*[@class="visualizar-descricao"]/descendant::*/text()')
 
         # 'caracteristicas' Section data
         var2_subtitle_list = []
         var2_value_list = []
+        caracteristicas_detalhes = {}
         for li in response.xpath('//*[@class="visualizacao-caracteristica-lista"]/li'):
-            var2_subtitle_list.append(li.xpath('.//*[@class="visualizacao-section-subtitle"]/text()').get())
-            var2_value_list.append(li.xpath('./ul/li/text()').getall())
+            key = li.xpath('.//*[@class="visualizacao-section-subtitle"]/text()').get()
+            value = li.xpath('./ul/li/text()').getall()
+            caracteristicas_detalhes[key] = value
+            #var2_subtitle_list.append(li.xpath('.//*[@class="visualizacao-section-subtitle"]/text()').get())
+            #var2_value_list.append(li.xpath('./ul/li/text()').getall())
+        item_loader.add_value('caracteristicas_detalhes', caracteristicas_detalhes)
 
         # 'endereco' Section data
-        address = response.xpath('//*[@class="visualizar-endereco-texto"]/text()').getall()
+        item_loader.add_xpath('address', '//*[@class="visualizar-endereco-texto"]/text()')
+        #address = response.xpath('//*[@class="visualizar-endereco-texto"]/text()').getall()
 
         # 'anunciante' Section data
+
         advertiser = response.xpath('//*[@class="visualizar-anunciante-info-nome"]').attrib['title']
+        item_loader.add_value('advertiser', advertiser)
+        #advertiser = response.xpath('//*[@class="visualizar-anunciante-info-nome"]/text()').getall()
+
+        # auxiliary data
+        item_loader.add_value('url', response.url)
+        item_loader.add_value('date_year', datetime.now().year)
+        item_loader.add_value('date_month', datetime.now().month)
+        item_loader.add_value('date_day', datetime.now().day)
+        #url = response.url
+        #date_year = datetime.now().year
+        #date_month = datetime.now().month
+        #date_day = datetime.now().day
+
+        item_loader.load_item()
 
         # item_loader.add_css("", "")
         yield item_loader.load_item()
@@ -95,101 +126,17 @@ process = CrawlerProcess()
 process.crawl(ImoveisSCSpider)
 process.start()
 
-# class ArticleSpider(scrapy.Spider):
-#     name = "medium_basic"
-#     custom_settings = {
-#         'AUTOTHROTTLE_ENABLED': True,
-#         'AUTOTHROTTLE_DEBUG': True,
-#         'DOWNLOAD_DELAY': 5,
-#         'ROBOTSTXT_OBEY': False,
-#     }
-#     def start_requests(self):
-#         urls = [
-#             'https://towardsdatascience.com/archive'
-#         ]
-#         for url in urls:
-#             yield scrapy.Request(url=url, callback=self.parse)
-
-#     def parse(self, response):
-#         year_pages = response.xpath('/html/body/div[1]/div[2]/div/div[3]/div[1]/div[1]/div/div[2]/*/a/@href').getall()
-#         if len(year_pages) != 0:
-#             yield from response.follow_all(year_pages, callback=self.parse_months)
-#         else:
-#             yield from self.parse_articles(response)
-    
-#     def parse_months(self, response):
-#         month_pages = response.xpath('/html/body/div[1]/div[2]/div/div[3]/div[1]/div[1]/div/div[3]/div/a/@href').getall()
-#         if len(month_pages) != 0:
-#             yield from response.follow_all(month_pages, callback=self.parse_days)
-#         else:
-#             yield from self.parse_articles(response)
-
-#     def parse_days(self, response):
-#         day_pages = response.xpath('/html/body/div[1]/div[2]/div/div[3]/div[1]/div[1]/div/div[4]/div/a/@href').getall()
-#         if len(day_pages) != 0:
-#             yield from response.follow_all(day_pages, callback=self.parse_articles)
-    
-#     def parse_articles(self, response):
-#         articles = response.xpath('/html/body/div[1]/div[2]/div/div[3]/div[1]/div[2]/*')
-#         if len(articles) != 0:
-#             for article in articles:
-#                 author = article.xpath('.//a[@data-action="show-user-card"]/text()').get()
-                
-#                 str_read_time = article.xpath('.//*[@class="readingTime"]/@title')[0].get()
-#                 int_read_time = str_read_time.split[0]
-
-#                 collection = article.xpath('.//a[@data-action="show-collection-card"]/text()').get()
-#                 title = article.xpath('.//h3[contains(@class, "title")]/text()').get()
-                
-#                 claps = article.xpath('.//button[@data-action="show-recommends"]/text()').get()
-#                 if claps == None:
-#                     claps = 0
-#                 responses = article.xpath('.//a[@class="button button--chromeless u-baseColor--buttonNormal"]/text()').get()
-#                 if responses == None:
-#                     responses = 0
-#                 subtitle_preview = article.xpath('.//h4[@name="previewSubtitle"]/text()').get()
-                
-#                 published_date = article.xpath('.//time/text()').get()
-#                 try:
-#                     date_object = datetime.strptime(published_date, "%b %d, %Y")
-#                     day = date_object.day
-#                     month = date_object.month
-#                     year = date_object.year
-#                 except:
-#                     date_object = datetime.strptime(published_date, "%b %d")
-#                     day = date_object.day
-#                     month = date_object.month
-#                     year = datetime.now().year
-
-#                 yield {
-#                     'author' : author,
-#                     'title' : title,
-#                     'subtitle preview' : subtitle_preview,
-#                     'collection' : collection,
-#                     'read time' : int_read_time,
-#                     'claps' : claps,
-#                     'responses' : responses,
-#                     'published date' : published_date,
-#                     'day' : day,
-#                     'month' : month,
-#                     'year' : year
-#                 }
-
-
-
-
-
-
-# url = "https://towardsdatascience.com/archive"
-# response = requests.get(url, verify = False)
-# parser = parser.fromstring(response.text)
-
-#parser.xpath('/html/body/div[1]/div[2]/div/div[3]/div[1]/div[1]/div/div[2]/*/a/@href')
-
-
 
 
 # bed - quartos
 # bath - suite
 # car - vagas
 # rule - m² (útil)
+# fullrule - m² (total)
+
+# var1 = var.replace('\n', '').replace('\t', '')
+# var2 = re.split('-| - |,|, ', var1)
+# var3 = [string.lstrip() for string in var2]
+# var4 = [string.rstrip() for string in var3]
+
+
