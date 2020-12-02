986d0d7e013a9161b23bc7bb6e1ad9bbcc617b9d
Doug <doug.sgrott@gmail.com>
2020-12-02 17:56:28 -0300

    Added randomized delay and region data to scraped catalog

--- realestate_scraper/spiders/imoveissc_catalogo_spider.py
+++ realestate_scraper/spiders/imoveissc_catalogo_spider.py
@@ -11,17 +11,21 @@ from itemloaders.processors import TakeFirst
 from datetime import datetime
 import sys
 
+import time
+import random
+
 sys.path.append("/home/user/PythonProj/Scraping/realestate_scraper/realestate_scraper")
 from items import ImoveisSCCatalogItem
 
 class ImoveisSCCatalogSpider(Spider):
     name = 'imoveis_sc_catalog'
-    start_urls = ['https://www.imoveis-sc.com.br/sao-bento-do-sul/comprar/casa']
+    # start_urls = ['https://www.imoveis-sc.com.br/regiao-serra/']
+    # start_urls = ['https://www.imoveis-sc.com.br/sao-bento-do-sul/comprar/casa']
     # start_urls = ['https://www.imoveis-sc.com.br/governador-celso-ramos/comprar/casa']  # LEVEL 1
     custom_settings = {
         'AUTOTHROTTLE_ENABLED': True,
         'AUTOTHROTTLE_DEBUG': True,
-        'DOWNLOAD_DELAY': 5,
+        'DOWNLOAD_DELAY': 3,
         'ROBOTSTXT_OBEY': False,
         'ITEM_PIPELINES': {
             'realestate_scraper.pipelines.SaveImoveisSCCatalogPipeline': 200,
@@ -29,27 +33,33 @@ class ImoveisSCCatalogSpider(Spider):
         }
     }
 
+    def __init__(self, *args, **kwargs):
+        super(ImoveisSCCatalogSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [kwargs.get('start_url')]
+
+
     # 1. FOLLOWING LEVEL 1
     def parse(self, response):
         i = 0
         for sel in response.xpath('//article[@class="imovel  "]'):
-            yield self.populate_item(sel)
+            yield self.populate_item(sel, response.url)
 
+        time.sleep(random.randint(3,7))
         yield self.paginate(response)
 
 
     # 2. SCRAPING LEVEL 1
-    def populate_item(self, selector):
+    def populate_item(self, selector, url):
         item_loader = ItemLoader(item=ImoveisSCCatalogItem(), selector=selector)
         item_loader.default_output_processor = TakeFirst()
-        #item_loader.add_xpath('title', '//h2[@class="imovel-titulo"]/a/text()')
         item_loader.add_xpath('title', './/h2[@class="imovel-titulo"]/a/meta[@itemprop="name"]/@content')
         item_loader.add_xpath('code', './/div[@class="imovel-extra"]/span/text()')
         item_loader.add_xpath('local', './/div[@class="imovel-extra"]/strong/text()')
         item_loader.add_xpath('description', './/p[@class="imovel-descricao"]/text()')
+        item_loader.add_value('region', url)
         item_loader.add_xpath('url', './/a[contains(@class, "btn-visualizar")]/@href')
         item_loader.add_value('date', datetime.now()) #.isoformat(' ')
-        #foo = item_loader.load_item()
+        foo = item_loader.load_item()
         return item_loader.load_item()
     
 
@@ -59,7 +69,7 @@ class ImoveisSCCatalogSpider(Spider):
         if next_page_url is not None:
             return response.follow(next_page_url, self.parse)
 
-# if __name__ == '__main__':
-#     process = CrawlerProcess(get_project_settings())
-#     process.crawl(ImoveisSCCatalogSpider)
-#     process.start()
+if __name__ == '__main__':
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(ImoveisSCCatalogSpider)
+    process.start()
