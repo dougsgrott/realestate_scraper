bb0dde510b61fe627b5630a95440e1fbd1d2b9de
Douglas Macedo <doug.sgrott@gmail.com>
2024-08-26 15:23:21 -0300

    Getting back and updating project

--- realestate_scraper/pw_wip.py
+++ realestate_scraper/pw_wip.py
@@ -8,6 +8,10 @@ import os
 import csv
 import numpy as np
 
+import sys
+import os
+
+
 class FooException(Exception):
     pass
 
@@ -15,6 +19,63 @@ class FooException(Exception):
 # #################################################
 #                    Scraper
 # #################################################
+
+import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst
+from datetime import datetime
+
+
+def parse_details(selector_list):
+    text_list = []
+    for html_string in selector_list:
+        soup = BeautifulSoup(html_string, 'html.parser')
+        text = soup.get_text()
+        text_list.append(text)
+    concat_text = '<br>'.join(text_list)
+    return concat_text
+
+
+def parse_amenities(html_string):
+    if html_string == []:
+        return 'none'
+    try:
+        soup = BeautifulSoup(html_string[0], 'html.parser')
+        text = soup.get_text()
+    except:
+        text = 'error'
+    return text
+
+
+def parse_values(html_string):
+    if html_string == []:
+        return 'none'
+    try:
+        soup = BeautifulSoup(html_string[0], 'html.parser')
+        text = soup.get_text()
+    except:
+        text = 'error'
+    return text
+
+
+def parse_target_url(url_list):
+    url = url_list[0]
+    if url.startswith('/'):
+        url = 'https://www.vivareal.com.br' + url
+    return url
+
+
+class VivaRealCatalogItem(scrapy.Item):
+    type = scrapy.Field()
+    address = scrapy.Field()
+    title = scrapy.Field()
+    details = scrapy.Field(input_processor=parse_details)
+    amenities = scrapy.Field(input_processor=parse_amenities) #input_processor=cleanText
+    values = scrapy.Field(input_processor=parse_values) #input_processor=MapCompose(getLocal)
+    target_url = scrapy.Field(input_processor=parse_target_url)
+    catalog_scraped_date = scrapy.Field()
+    is_target_scraped = scrapy.Field()
+
 
 class ScraperVivaReal(object):
     def __init__(self, page, writer=None):
@@ -64,6 +125,24 @@ class ScraperVivaReal(object):
             'target_url': target_url,
         }
         return item
+
+    def populate_catalog(self, selector, url):
+        catalog_loader = ItemLoader(item=VivaRealCatalogItem(), selector=selector)
+        catalog_loader.default_output_processor = TakeFirst()
+
+        catalog_loader.add_value('type', "catalog")
+        catalog_loader.add_xpath('address', './/*[@class="property-card__address"]/text()')
+        catalog_loader.add_xpath('title', './/*[contains(@class, "property-card__title")]/text()')
+        catalog_loader.add_xpath('details', './/*[contains(@class, "property-card__detail-item")]')
+        catalog_loader.add_xpath('amenities', './/*[contains(@class, "property-card__amenities")]')
+        catalog_loader.add_xpath('values', './/*[contains(@class, "property-card__values")]')
+        catalog_loader.add_xpath('target_url', './/*[contains(@class, "property-card__carousel")]/a/@href')
+        # catalog_loader.add_value('catalog_scraped_date', datetime.now())
+        catalog_loader.add_value('catalog_scraped_date', datetime.now().isoformat())
+        catalog_loader.add_value('is_target_scraped', 0)
+        loaded_item = catalog_loader.load_item()
+        return loaded_item
+
 
     def _scrape_target(): pass
 
@@ -116,18 +195,41 @@ class CsvReader(DataLoader):
 
 
 class CsvWriter(DataWriter):
-    def __init__(self, file_path, write_in_batches=False, append=True):
+    def __init__(self, file_path, reader=None, write_in_batches=False, avoid_duplicates=True,  duplicate_columns=[]): #append=True, 
+        self.reader = reader
         self.file_path = file_path
         self.write_in_batches = write_in_batches
-        self.append = append
+        self.avoid_duplicates = avoid_duplicates
+        self.duplicate_columns = duplicate_columns
+        # self.append = append
+
+    def load_data(self):
+        data = self.reader.load_data()
+        return data
 
     def _write(self, data: pd.DataFrame):
         timestamp = time.strftime("%Y%m%d-%H%M%S")
         file_name = f"vivareal_{timestamp}.csv"
         data.to_csv(os.path.join(self.file_path, file_name))
 
+    def filter_duplicates(self, data, new_entries):
+        if data.empty:
+            return new_entries
+        columns = self.duplicate_columns
+        if self.duplicate_columns == []:
+            columns = data.columns
+        bool_mask = np.any([~new_entries[col].isin(data[col]) for col in columns], axis=0)
+        new_entries = new_entries[bool_mask]
+        return new_entries
+
     def write(self, items: dict):
         new_entries = pd.DataFrame.from_dict(data=items, orient='columns')
+        if new_entries.empty:
+            return
+        if self.reader is not None:
+            data = self.load_data()
+            if self.avoid_duplicates:
+                new_entries = self.filter_duplicates(data, new_entries)
         self._write(new_entries)
 
 
@@ -170,6 +272,8 @@ class CsvAppender(DataAppender):
 
     def write(self, items: list):
         new_entries = pd.DataFrame.from_dict(data=items, orient='columns')
+        if new_entries.empty:
+            return
         data = self.load_data()
         if self.avoid_duplicates:
             new_entries = self.filter_duplicates(data, new_entries)
@@ -186,25 +290,35 @@ class CsvAppender(DataAppender):
 if __name__ == "__main__":
     LOGGER = logging.getLogger(__name__)
 
+    curr_path = os.path.dirname(os.path.realpath(__file__))
+    base_path = os.path.abspath(os.path.join(curr_path, os.pardir))
+    # sys.path.append(base_path)
+    file_path = os.path.join(base_path, 'data') #'/media/user/Novo volume/Python/Secondary/realestate_scraper/data'
+    file_name = 'foo.csv'
+
     with sync_playwright() as p:
+        # use navigator as firefox
+        # navigator = p.firefox.launch(headless=False)
         navigator = p.chromium.launch(headless=False)
         page = navigator.new_page()
         # page.goto("https://www.vivareal.com.br/aluguel/espirito-santo/vila-velha/bairros/itapua/#onde=Brasil,Esp%C3%ADrito%20Santo,Vila%20Velha,Bairros,Itapu%C3%A3,,,,BR%3EEspirito%20Santo%3ENULL%3EVila%20Velha%3EBarrios%3EItapua,,,;,Esp%C3%ADrito%20Santo,Vila%20Velha,Bairros,Praia%20da%20Costa,,,neighborhood,BR%3EEspirito%20Santo%3ENULL%3EVila%20Velha%3EBarrios%3EPraia%20da%20Costa,-20.330616,-40.290992,&tipos=apartamento_residencial,flat_residencial,kitnet_residencial")
         page.goto("https://www.vivareal.com.br/aluguel/espirito-santo/vila-velha/apartamento_residencial/#onde=Brasil,Esp%C3%ADrito%20Santo,Vila%20Velha,,,,,,BR%3EEspirito%20Santo%3ENULL%3EVila%20Velha,,,;,Esp%C3%ADrito%20Santo,Vila%20Velha,Bairros,Praia%20de%20Itaparica,,,neighborhood,BR%3EEspirito%20Santo%3ENULL%3EVila%20Velha%3EBarrios%3EPraia%20de%20Itaparica,,,&tipos=apartamento_residencial,flat_residencial,kitnet_residencial")
-        
-        
-        file_path = '/media/user/Novo volume/Python/Secondary/realestate_scraper/data'
-        file_name = 'foo.csv'
 
         # writer = CsvWriter(
         #     file_path=file_path,
         #     write_in_batches=False,
         # )
 
+        # writer = CsvAppender(
+        #     reader=CsvReader(file_path=file_path, file_name=file_name),
+        #     avoid_duplicates=True,
+        #     duplicate_columns=['address', 'title', 'price'],
+        #     # mode='a'
+        # ) 
         writer = CsvAppender(
             reader=CsvReader(file_path=file_path, file_name=file_name),
             avoid_duplicates=True,
-            # duplicate_columns=['link'],
+            duplicate_columns=['address', 'title', 'price'],
             # mode='a'
         )
 
@@ -227,6 +341,33 @@ if __name__ == "__main__":
 
 
 # if __name__ == "__main__":
+#     filepath_1 = '/media/user/Novo volume/Python/Secondary/realestate_scraper/test_data'
+#     filepath_2 = '/media/user/Novo volume/Python/Secondary/realestate_scraper/test_data/foo'
+#     filename_1 = 'vivareal_ab.csv'
+#     filename_2 = 'vivareal_b.csv'
+
+#     reader_1 = CsvReader(filepath_1, filename_1)
+#     reader_2 = CsvReader(filepath_1, filename_2)
+
+#     # data_1 = reader_1.load_data()
+#     data_2 = reader_2.load_data()
+
+#     appender = CsvWriter(filepath_2, reader_1, avoid_duplicates=False) #, duplicate_columns=['address', 'title'])
+#     appender.write(data_2)
+
+#     print("EOL")
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     """
+#     vivareal_ab is initially equal to vivareal_a. As this main runs, vivareal_b is added to vivareal_ab.
+#     If avoid_duplicates=True, no duplicates will be appended to vivareal_ab.
+#     If avoid_duplicates=False, duplicates will be appended to vivareal_ab.
+#     """
 #     filepath_1 = '/media/user/Novo volume/Python/Secondary/realestate_scraper/test_data'
 #     filename_1 = 'vivareal_ab.csv'
 #     filename_2 = 'vivareal_b.csv'
